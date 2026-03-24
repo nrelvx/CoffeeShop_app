9d98aa62049e27b9b1f0ddc39922a5addc028f34
Veronika Sokolova <Sveronikasveronika01@gmail.com>
2026-03-24 17:17:14 +0200

    UPD

--- app/src/main/java/com/example/coffeeshop/MainActivity.java
+++ app/src/main/java/com/example/coffeeshop/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.coffeeshop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        Intent intent = new Intent(this, LoginActivity.class);
+        finish();
     }
 }
